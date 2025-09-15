79869ef6111f02433c236afc42462df7dea64195
Komal0patel <komal.1620patel@gmail.com>
2025-09-15 15:24:17 +0530

    origin

--- src/main/java/com/komal/template_backend/repo/HeroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.komal.template_backend.repo;
-import com.komal.template_backend.model.Hero;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface HeroRepository extends MongoRepository<Hero, String>{
-}
--- src/main/java/com/komal/template_backend/repo/PressReleaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.komal.template_backend.repo;
-
-import com.komal.template_backend.model.PressRelease;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PressReleaseRepository extends MongoRepository<PressRelease, String> {
-}
